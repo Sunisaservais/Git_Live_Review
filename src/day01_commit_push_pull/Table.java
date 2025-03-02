189907588b25136b7fbc156e5c2c7e41d8526624
sunisaservais <sunisaservais@gmaill.com>
2025-03-02 11:54:23 -0500

    Overloaded multiplicationTable method to print table for number 1-10

--- src/day01_commit_push_pull/Table.java
+++ src/day01_commit_push_pull/Table.java
@@ -5,6 +5,7 @@ public class Table {
     public static void main(String[] args) {
 
         multiplicationTable(5);
+        multiplicationTable();
     }
 
  /*
@@ -14,9 +15,22 @@ public class Table {
     prints multiplication table for the number
  */
 
-    public static void multiplicationTable(int num){
-        for (int i = 0; i <= 12 ; i++) {
-            System.out.println(num + " x " + i + "\t= " + (num*i));
+    public static void multiplicationTable(int num) {
+        for (int i = 0; i <= 12; i++) {
+            System.out.println(num + " x " + i + "\t= " + (num * i));
+        }
+    }
+
+/*
+    Overloaded Method: multiplicationTable
+    Param: num
+    Return: void
+    prints multiplication table for the number 1-9
+*/
+    public static void multiplicationTable() {
+        for (int i = 1; i <= 10; i++) {
+            multiplicationTable(i);
+            System.out.println("___________");
         }
     }
 }
